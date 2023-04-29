46844f5515268e81afdb7b29c885c2de3f274fcb
VAIBHAV <68508494+vaibhavd2103@users.noreply.github.com>
2023-04-29 20:06:11 +0530

    env port

--- index.js
+++ index.js
@@ -23,82 +23,82 @@ var multer = require("multer");
 const Image = require("./models/Image");
 
 var storage = multer.diskStorage({
-  destination: "uploads",
-  filename: (req, file, cb) => {
-    cb(null, file.originalName + "-" + Date.now());
-  },
+	destination: "uploads",
+	filename: (req, file, cb) => {
+		cb(null, file.originalName + "-" + Date.now());
+	},
 });
 
 var upload = multer({ storage: storage }); // upload images
 
 app.post("/uploadImage", upload.single("image"), (req, res) => {
-  console.log("--------------->>>>>>>>>>>>", req.file);
-  const newImage = new Image({
-    name: req.body.name,
-    desc: req.body.desc,
-    img: {
-      data: req.file.filename.toString("base64"),
-      contentType: "image/png",
-    },
-  });
+	console.log("--------------->>>>>>>>>>>>", req.file);
+	const newImage = new Image({
+		name: req.body.name,
+		desc: req.body.desc,
+		img: {
+			data: req.file.filename.toString("base64"),
+			contentType: "image/png",
+		},
+	});
 
-  newImage.save().then((err, item) => {
-    if (err) {
-      console.log(err);
-    }
-    // item.save();
-    // res.redirect("/");
-    res.status(200).json({ image: item });
-  });
+	newImage.save().then((err, item) => {
+		if (err) {
+			console.log(err);
+		}
+		// item.save();
+		// res.redirect("/");
+		res.status(200).json({ image: item });
+	});
 });
 
 const connectMongo = async () => {
-  try {
-    await mongoose.set("strictQuery", false);
-    await mongoose.connect(config.dbUrl);
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.log("Error in connection to DB", error);
-  }
+	try {
+		await mongoose.set("strictQuery", false);
+		await mongoose.connect(config.dbUrl);
+		console.log("MongoDB connected successfully");
+	} catch (error) {
+		console.log("Error in connection to DB", error);
+	}
 };
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "DELETE"],
-  },
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST", "DELETE"],
+	},
 });
 
 io.on("connection", (socket) => {
-  console.log("User connected : ", socket.id);
+	console.log("User connected : ", socket.id);
 
-  socket.emit("connected", "User online");
+	socket.emit("connected", "User online");
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(
-      `User with ID : ${socket.id} joined the room with ID : ${data}`
-    );
-  });
+	socket.on("join_room", (data) => {
+		socket.join(data);
+		console.log(
+			`User with ID : ${socket.id} joined the room with ID : ${data}`
+		);
+	});
 
-  socket.on("send_message", (message) => {
-    console.log("received message : ", message);
-    // io.emit("receive_message", message);
-    io.to(message?.chatRoomId).emit("receive_message", message);
-    // socket.to(message?.roomId).emit("receive_message", message);
-  });
+	socket.on("send_message", (message) => {
+		console.log("received message : ", message);
+		// io.emit("receive_message", message);
+		io.to(message?.chatRoomId).emit("receive_message", message);
+		// socket.to(message?.roomId).emit("receive_message", message);
+	});
 
-  socket.on("disconnect", () => {
-    console.log(`User disconnected ${socket.id}`);
-  });
+	socket.on("disconnect", () => {
+		console.log(`User disconnected ${socket.id}`);
+	});
 });
 
-server.listen(config.port, async () => {
-  console.log(
-    `Server running on port ${config.port} (i.e) http://localhost:${config.port}`
-  );
-  await connectMongo();
-  await routes(app);
+server.listen(process.env.PORT || config.port, async () => {
+	console.log(
+		`Server running on port ${config.port} (i.e) http://localhost:${config.port}`
+	);
+	await connectMongo();
+	await routes(app);
 });
 
 module.exports = app;
